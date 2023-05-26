4d21481fc968609c594472fc143efedbcf75d498
Sumitsah1312 <sks13122003@gmail.com>
2023-05-26 22:29:49 +0530

    bug fixing

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [data, setData] =useState({
     celcius:10,
     name:'London',
-    Humidity:10,
+    humidity:10,
     speed:2
   })
 
@@ -40,34 +40,35 @@ function App() {
 
   return (
     <>
-      <div className="weatherbg " >
-         <h1 className="word">Weather app</h1>
+      <div className="bg-[url('https://nordicapis.com/wp-content/uploads/How-to-Build-an-API-Driven-Weather-App-1024x576.png')] text-center min-h-half justify-center" >
+         <h1 className="text-white font-bold mx-auto text-3xl p-20 ">Weather app</h1>
 
 
-        <input type="text" className='enter' placeholder='City Name' onChange={e=> setName(e.target.value)}/>
 
-        <button type="button" className="btn" onClick={handleclick}>
+        <input type="text" className="text-xl rounded-xl px-4 py-1 border-gray-100 font-semibold uppercase bg-slate-200  " placeholder='City Name' onChange={e=> setName(e.target.value)}/>
+
+        <button type="button" className="m-10 bg-orange-300 p-1 rounded-lg  " onClick={handleclick}>
           Get Weather
         </button>
       </div>
 
 
 
-      <div className='answer'>
-          <img className='wicon'
+      <div className='bg-slate-300 justify-center place-content-center  w-1/3 rounded-xl p-5 border-gray-100   text-center  mx-[30%] my-3'>
+          <img className='max-h-44 mx-auto '
           src="https://blog.weatherbit.io/content/images/2022/12/blog4.png" alt="" />
 
-          <div className="error">
+          <div className="mx-auto my-2 text-red-600 ">
             <p>{error}</p>
           </div>
 
-          <h1 className='temp'>{Math.round(data.celcius)} °C</h1>
+          <h1 className='text-2xl font-semibold my-1.5'>{Math.round(data.celcius)} °C</h1>
           <p className='city'>{data.name}</p>
 
 
           <div className='output'>
               <div>
-                <p className='humidity'>Humidity : {data.humidity} </p>
+                <p className='py-2'>Humidity : {data.humidity} </p>
                 
               </div>
               <div>
